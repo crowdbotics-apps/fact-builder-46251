4bd388e20c4fac86197ce241c6b2460da5fd7d40
Crowdbotics <team@crowdbotics.com>
2024-02-12 02:54:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fact-builder-46251",
+  url: "https://fact-builder-46251.botics.co",
+  api: "https://fact-builder-46251.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
